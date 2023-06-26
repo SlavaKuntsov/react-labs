13679cfc51b8a224c55003b904a95685b3f4c31e
Slava <kuncovs19@gmail.com>
2023-06-27 00:58:31 +0300

    Updates

--- src/components/layout/Layout.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import style from "./Layout.module.scss";
-
-export default function Layout({ children }) {
-    return <div className={style.wrapper}>{children}</div>;
-}
